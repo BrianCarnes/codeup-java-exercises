aab7f1dd3f59c7dbdd6434597de02ce94dd1cf2f
Brian Carnes <carnes.brian.a@gmail.com>
2022-08-03 10:20:20 -0500

    stringExercise complete.

--- src/stringExercise.java
+++ src/stringExercise.java
@@ -1,5 +1,14 @@
 public class stringExercise {
     public static void main(String[] args) {
-        String floyd = ""
+        String floyd = "We don't need no education\n" +
+                "We don't need no thought control";
+        String checkIt = "Check \"this\" out!, \"s inside of \"s!";
+        String dos = "In windows, the main drive is usually C:\\";
+        String slash = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(floyd);
+        System.out.println(checkIt);
+        System.out.println(dos);
+        System.out.println(slash);
     }
 }
